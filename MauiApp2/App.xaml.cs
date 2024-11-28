9d0aa5e7dd0d721c25cbb5ac9f7a3f626edd3495
rahulsabu27 <rahulsabu27@gmail.com>
2024-11-28 16:37:48 +0530

    Add project files.

---
+++ MauiApp2/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace MauiApp2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+          
+        }
+    }
+}
---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            string from = "rahulsabu27sep@gmail.com";
+            string pass = "rjjdpguackhljccb";
+            string to = "sidharths31434@gmail.com";
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(from);
+            message.Subject = "Test";
+            message.To.Add(new MailAddress(to));
+            message.Body = "Test";
+            message.IsBodyHtml = false;
+            var smt = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(from, pass)
+                 ,
+                EnableSsl = true,
+            };
+            smt.Send(message);
+        }
+    }
+
+}
